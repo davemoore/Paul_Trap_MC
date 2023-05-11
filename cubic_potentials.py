325c6eb9e142e6a9197599034d30dfbbedda3145
David Moore <david.c.moore@yale.edu>
2023-05-11 11:05:30 -0400

    adaptive step near sphere

--- cubic_potentials.py
+++ cubic_potentials.py
@@ -1,18 +1,20 @@
 ## helper functions to calcuate potentials fields in a cube analytically
 import numpy as np
+import matplotlib.pyplot as plt
 
 epsilon_0 = 8.85e-12 ## F/m
 e_to_Coul = 1.6e-19 ## Coulombs
 cm_to_m = 1e-2 ## convert cm to SI
 k = 1/(4*np.pi*epsilon_0) * e_to_Coul * 1/cm_to_m
 
-def V(x,y,z, a=1.0, V_0=1.0, n_max=20, m_max=20, q_sphere = 0, sphere_rad = 1.5e-4):
+def V(x,y,z, a=1.0, V_0=1.0, n_max=20, m_max=20, q_ion = 2, q_sphere = 0, sphere_rad = 1.5e-4):
     """ Function to calculate the potential for a cube with two sides (in the x direction) at potential V_0
             x,y,z -- coordinates to evaluate (can be arrays, units must be cm!)
             a -- side length (units must be cm!)
             V_0 -- potential on faces at x = +/- a/2
             n_max, m_max -- max terms to sum the series over in y and z directions
             q_sphere -- charge of sphere (in electrons)
+            q_ion -- charge of ion (in electrons)
             sphere_rad -- sphere radius (units must be_cm!)
             
         returns an array of the size of x,y,z
@@ -32,13 +34,14 @@ def V(x,y,z, a=1.0, V_0=1.0, n_max=20, m_max=20, q_sphere = 0, sphere_rad = 1.5e
     
     return prefac*Vout + Vsphere
 
-def E(x,y,z, a=1.0, V_0=1.0, n_max=20, m_max=20, q_sphere = 0, sphere_rad = 1.5e-4):
+def E(x,y,z, a=1.0, V_0=1.0, n_max=20, m_max=20, q_ion = 2, q_sphere = 0, sphere_rad = 1.5e-4):
     """ Function to calculate the electric field for a cube with two sides (in the x direction) at potential V_0
             x,y,z -- coordinates to evaluate (can be arrays)
             a -- side length
             V_0 -- potential on faces at x = +/- a/2
             n_max, m_max -- max terms to sum the series over in y and z directions
             q_sphere -- charge of sphere (in electrons)
+            q_ion -- charge of ion (in electrons)
             sphere_rad -- sphere radius (units must be_cm!)
 
         returns 3 arrays Ex,Ey,Ez of the size of x,y,z in V/cm!
@@ -83,33 +86,114 @@ def track_particle(x0, mu, fAC, VAC, q_sphere, sphere_rad, max_t=0.1, side_lengt
         returns trajectory [x,y,z,t]
     '''
     
-    tstep = 0.2/fAC ## time step as a fraction of sample time (10x)
-    tvec = np.arange(0, max_t, tstep) ## time vector in s
+    MAX_STEPS = int(max_t*fAC*100) ## maximum steps to take ever
+    high_res_rad = 1e-2 ## cm, when in the radius step by at most 0.5 um
+    MAX_STEP_SIZE = 0.5e-4 ## cm, step by at most 0.5 um
+
+    tstep = 0.1/fAC ## time step as a fraction of sample time (10x)
+    curr_t = 0
     delta = np.random.rand()*2*np.pi
 
-    volt_vec = VAC*np.sin(2*np.pi*fAC*tvec + delta)
-
     trajectory = []
-    trajectory.append([x0[0], x0[1], x0[2], tvec[0]]) # put initial position in trajectory
-    print("Working on %d points in trajectory:"%len(tvec))
-    for j,t in enumerate(tvec):
+    trajectory.append([x0[0], x0[1], x0[2], curr_t]) # put initial position in trajectory
+    print("Working on %d points in trajectory:"%MAX_STEPS)
+    for j in range(MAX_STEPS):
 
         if(j%10000 == 0): print(j)
 
-        curr_volt = volt_vec[j] ## voltage at this time
+        curr_volt = VAC*np.sin(2*np.pi*fAC*curr_t + delta) ## voltage at this time
         curr_pos = np.array(trajectory[-1][0:3]) ## last position
 
         ex, ey, ez = E(curr_pos[0:1], curr_pos[1:2], curr_pos[2:], a=side_length, 
                        V_0=curr_volt, q_sphere = q_sphere, sphere_rad = sphere_rad)
         
-        ctm = tstep*mu
-        dx, dy, dz = ctm*ex[0], ctm*ey[0], ctm*ez[0]
+        if(np.sqrt(np.sum(curr_pos**2)) > high_res_rad):
+            ctm = tstep*mu
+            dx, dy, dz = ctm*ex[0], ctm*ey[0], ctm*ez[0]
+            curr_t += tstep
+        else:
+            ## if close to the sphere take an adaptive step that moves by a max size
+            enorm = np.sqrt(ex[0]**2 + ey[0]**2 + ez[0]**2)/MAX_STEP_SIZE
+            dx, dy, dz = ex[0]/enorm, ey[0]/enorm, ez[0]/enorm
+            variable_step = 1/(enorm * mu)
+            curr_t += variable_step
     
         updated_pos = [curr_pos[0]+dx, curr_pos[1]+dy, curr_pos[2]+dz]
-        trajectory.append([updated_pos[0], updated_pos[1], updated_pos[2], t])
+        trajectory.append([updated_pos[0], updated_pos[1], updated_pos[2], curr_t])
 
         ## break if we are in the sphere
         if(np.sqrt(np.sum(np.array(updated_pos)**2)) <= sphere_rad):
             break
 
     return np.array(trajectory)
+
+def plot_sphere(ax, r, c, alph=0.5):
+    u = np.linspace(0, 2 * np.pi, 100)
+    v = np.linspace(0, np.pi, 100)
+    x = r*np.outer(np.cos(u), np.sin(v))
+    y = r*np.outer(np.sin(u), np.sin(v))
+    z = r*np.outer(np.ones(np.size(u)), np.cos(v))
+
+    # plot sphere with transparency
+    ax.plot_surface(x, y, z, alpha=alph, color=c)
+
+def plot3Dtraj(traj, sphere_rad = 1.5e-4, title="", nmax = -1, zoom_range=5e-3):
+
+    ### plot the trajoectory in 3D
+    if(nmax < 0):
+        nmax = len(traj[:,0])
+
+    # Create a 3D plot
+    fig = plt.figure(figsize=(12,9), facecolor='white', layout='constrained')
+
+    subfigs = fig.subfigures(2, 1, hspace=0.07, height_ratios=[2.5, 1])
+
+    ax3d = subfigs[0].add_subplot(1,2,1, projection='3d')
+    ax3d_in = subfigs[0].add_subplot(1,2,2, projection='3d')
+    ax2d = subfigs[1].subplots(1,3)
+
+    ######## first the 3d plot zoomed out ##################
+    cvec = traj[:nmax,3] #/np.max(traj[:,3])
+    cmap = plt.get_cmap("jet")
+    cb=ax3d.scatter3D(traj[:nmax,0], traj[:nmax,1], traj[:nmax,2], c=cvec, cmap=cmap, s=1, ls='--')
+    ax3d.plot(traj[:nmax,0], traj[:nmax,1], traj[:nmax,2], 'k', lw=1, alpha=0.4)
+
+    cbar = plt.colorbar(cb)
+    cbar.set_label('Time [s]')
+
+    # Add labels and title
+    ax3d.set_xlabel('X [cm]')
+    ax3d.set_ylabel('Y [cm]')
+    ax3d.set_zlabel('Z [cm]')
+    ax3d.set_xlim(-1,1)
+    ax3d.set_ylim(-1,1)
+    ax3d.set_zlim(-1,1)
+    ax3d.set_title(title)
+
+    ### now 3d plot zoomed in to end ###############
+
+    ## find the index where we reach within 50 um of trajectory end
+    rel_traj = traj[:,:3]-traj[-1,:3]
+    nsamples = np.where(np.sqrt(np.sum(rel_traj**2, axis=1))<zoom_range)[0][0]
+
+    cvec = traj[nsamples:,3]
+    cb=ax3d_in.scatter3D(traj[nsamples:,0], traj[nsamples:,1], traj[nsamples:,2], c=cvec, cmap=cmap, s=1, ls='--')
+    ax3d_in.plot(traj[nsamples:,0], traj[nsamples:,1], traj[nsamples:,2], 'k', lw=1, alpha=0.4)
+
+    cbar = plt.colorbar(cb)
+    cbar.set_label('Time [s]')
+
+    plot_sphere(ax3d_in, sphere_rad, 'gray', alph=0.5)
+
+    # Add labels and title
+    ax3d_in.set_xlabel('X [cm]')
+    ax3d_in.set_ylabel('Y [cm]')
+    ax3d_in.set_zlabel('Z [cm]')
+    ax_extent = np.max((np.ptp(traj[nsamples:,0]), np.ptp(traj[nsamples:,1]), np.ptp(traj[nsamples:,2])))
+    cl,ch = np.median(traj[nsamples:,0])-ax_extent/2, np.median(traj[nsamples:,0])+ax_extent/2
+    ax3d_in.set_xlim(cl,ch)
+    cl,ch = np.median(traj[nsamples:,1])-ax_extent/2, np.median(traj[nsamples:,1])+ax_extent/2
+    ax3d_in.set_ylim(cl,ch)
+    cl,ch = np.median(traj[nsamples:,2])-ax_extent/2, np.median(traj[nsamples:,2])+ax_extent/2
+    ax3d_in.set_zlim(cl,ch)
+    ax3d_in.set_title("Zoom to end of trajectory")

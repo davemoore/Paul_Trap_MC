5d2ef2b4c7b1bfdd1be94950da31a2a091fbc8ea
David Moore <david.c.moore@yale.edu>
2023-05-15 15:04:04 -0400

    minor

--- cubic_potentials.py
+++ cubic_potentials.py
@@ -1,6 +1,7 @@
 ## helper functions to calcuate potentials fields in a cube analytically
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import odeint
 
 epsilon_0 = 8.85e-12 ## F/m
 e_to_Coul = 1.6e-19 ## Coulombs
@@ -72,7 +73,46 @@ def E(x,y,z, a=1.0, V_0=1.0, n_max=20, m_max=20, q_ion = 2, q_sphere = 0, sphere
 
     return prefac*Ex + Es_x, prefac*Ey + Es_y, prefac*Ez + Es_z
 
-def track_particle(x0, mu, fAC, VAC, q_sphere, sphere_rad, max_t=0.1, side_length=5):
+def Eode(x,y,z, a=1.0, V_0=1.0, n_max=20, m_max=20, q_ion = 2, q_sphere = 0, sphere_rad = 1.5e-4):
+    """ Function to calculate the electric field for a cube with two sides (in the x direction) at potential V_0
+            x,y,z -- coordinates to evaluate (can be arrays)
+            a -- side length
+            V_0 -- potential on faces at x = +/- a/2
+            n_max, m_max -- max terms to sum the series over in y and z directions
+            q_sphere -- charge of sphere (in electrons)
+            q_ion -- charge of ion (in electrons)
+            sphere_rad -- sphere radius (units must be_cm!)
+
+        returns 3 arrays Ex,Ey,Ez of the size of x,y,z in V/cm!
+    """
+
+    prefac = -16*V_0/(a * np.pi)
+    Ex,Ey,Ez = 0,0,0
+    
+    ## field from sphere (if desired)
+    r = np.sqrt(x**2 + y**2 + z**2)
+    Esphere = k * q_sphere/r**2
+    if(r<=sphere_rad):
+        Esphere = 0 ## no field inside
+    Es_x, Es_y, Es_z = Esphere*x/r, Esphere*y/r, Esphere*z/r 
+
+    for n in range(1,n_max,2): ## sum over odd integers from 1 to n_max
+        for m in range(1,m_max,2): ## sum over odd integers from 1 to m_max
+            
+            ## precalculate common factors to save time
+            smn = np.sqrt(n**2 + m**2)
+            sy, cy = np.sin(n*np.pi*(y/a + 0.5)), np.cos(n*np.pi*(y/a + 0.5))
+            sz, cz = np.sin(m*np.pi*(z/a + 0.5)), np.cos(m*np.pi*(z/a + 0.5))
+            sx, cx = np.sinh(np.pi*smn*x/a), np.cosh(np.pi*smn*x/a)
+            const = np.sinh(np.pi*smn/2)
+
+            Ex = Ex + smn/(1.0*n*m) * sy * sz * sx / const
+            Ey = Ey + 1.0/m * cy * sz * cx / const
+            Ez = Ez + 1.0/n * sy * cz * cx / const
+
+    return prefac*Ex + Es_x, prefac*Ey + Es_y, prefac*Ez + Es_z
+
+def track_particle(x0, mu, fAC, VAC, q_sphere, sphere_rad, max_t=0.1, side_length=5, oversamp=10):
     ''' Track a particle through the E-field
         x0 -- initial position (x,y,z) in cm
         mu -- mobility in cm^2/(s*V)
@@ -82,6 +122,7 @@ def track_particle(x0, mu, fAC, VAC, q_sphere, sphere_rad, max_t=0.1, side_lengt
         side_length -- cube side length in cm
         q_sphere -- charge of sphere (in electrons)
         sphere_rad -- sphere radius (units must be_cm!)
+        oversamp -- over sampling of the oscillation frequency
 
         returns trajectory [x,y,z,t]
     '''
@@ -90,18 +131,20 @@ def track_particle(x0, mu, fAC, VAC, q_sphere, sphere_rad, max_t=0.1, side_lengt
     high_res_rad = 1e-2 ## cm, when in the radius step by at most 0.5 um
     MAX_STEP_SIZE = 0.5e-4 ## cm, step by at most 0.5 um
 
-    tstep = 0.1/fAC ## time step as a fraction of sample time (10x)
+    tstep = 1/(oversamp * fAC) ## time step as a fraction of sample time (10x)
     curr_t = 0
-    delta = np.random.rand()*2*np.pi
+    delta = 0 #np.random.rand()*2*np.pi
 
     trajectory = []
     trajectory.append([x0[0], x0[1], x0[2], curr_t]) # put initial position in trajectory
     print("Working on %d points in trajectory:"%MAX_STEPS)
     for j in range(MAX_STEPS):
 
-        if(j%10000 == 0): print(j)
+        if(j%10000 == 0): print("Curr iter %d, curr time %f"%(j, curr_t))
 
-        curr_volt = VAC*np.sin(2*np.pi*fAC*curr_t + delta) ## voltage at this time
+        #curr_volt = VAC*np.sin(2*np.pi*fAC*curr_t + delta) ## voltage at this time
+        ## time averaged voltage over the step time
+        curr_volt = VAC*np.sin(np.pi*fAC*tstep)*np.sin(np.pi*fAC*(2*curr_t + tstep))
         curr_pos = np.array(trajectory[-1][0:3]) ## last position
 
         ex, ey, ez = E(curr_pos[0:1], curr_pos[1:2], curr_pos[2:], a=side_length, 
@@ -122,10 +165,64 @@ def track_particle(x0, mu, fAC, VAC, q_sphere, sphere_rad, max_t=0.1, side_lengt
         trajectory.append([updated_pos[0], updated_pos[1], updated_pos[2], curr_t])
 
         ## break if we are in the sphere
-        if(np.sqrt(np.sum(np.array(updated_pos)**2)) <= sphere_rad):
+        if(np.sqrt(np.sum(np.array(updated_pos)**2)) <= sphere_rad or curr_t>max_t):
             break
 
     return np.array(trajectory)
+
+def part_accel(y, t, mu, fAC, VAC, q_sphere, sphere_rad, ctm, side_length=5, delta=np.pi/2):
+
+    x, y, z, vx, vy, vz = y
+
+    ## calculate E-field
+    V = VAC*np.sin(2*np.pi*fAC*t + delta)
+    ex, ey, ez = Eode(x,y,z, a=side_length, V_0=V, q_sphere=q_sphere, sphere_rad = sphere_rad)
+
+    #ax, ay, az = ctm*ex-ctm*vx/mu, ctm*ey-ctm*vy/mu, ctm*ez-ctm*vz/mu
+    ## we reach terminal velocity very quickly (~20 ns) so for all time steps bigger than that 
+    ## ignore the inertial term
+    ax, ay, az = ctm*ex-ctm*vx/mu, ctm*ey-ctm*vy/mu, ctm*ez-ctm*vz/mu
+    dydt = [vx, vy, vz, ax, ay, az]
+    print(ex, ey, ez)
+    print(dydt)
+
+    return dydt
+
+def part_viscous(y, t, mu, fAC, VAC, q_sphere, sphere_rad, side_length=5, delta=np.pi/2):
+
+    x, y, z = y
+
+    ## calculate E-field
+    V = VAC*np.sin(2*np.pi*fAC*t + delta)
+    ex, ey, ez = Eode(x,y,z, a=side_length, V_0=V, q_sphere=q_sphere, sphere_rad = sphere_rad)
+
+    ## we reach terminal velocity very quickly (~20 ns) so just set the velocity to the terminal at the field
+    vx, vy, vz = mu*ex, mu*ey, mu*ez
+    dydt = [vx, vy, vz]
+
+    return dydt
+
+def track_particle_odeint(x0, mu, fAC, VAC, q_sphere, sphere_rad, ctm=8.9e9, max_t=0.1, side_length=5, oversamp=10):
+    ''' Track a particle through the E-field using built in odeint from python
+        x0 -- initial position (x,y,z) in cm
+        mu -- mobility in cm^2/(s*V)
+        fAC -- frequency of AC field in Hz
+        max_t -- maximum time in s
+        VAC -- voltage amplitude in V
+        side_length -- cube side length in cm
+        q_sphere -- charge of sphere (in electrons)
+        sphere_rad -- sphere radius (units must be_cm!)
+        oversamp -- over sampling of the oscillation frequency
+
+        returns trajectory [x,y,z,t]
+    '''
+
+    tvec = np.arange(0, max_t, 1/(oversamp*fAC))
+    y0 = [x0[0], x0[1], x0[2]] ## initial params
+
+    sol = odeint(part_viscous, y0, tvec, args=(mu, fAC, VAC, q_sphere, sphere_rad))
+    traj = np.hstack((sol[:,:3], tvec[np.newaxis].T))
+    return traj
 
 def plot_sphere(ax, r, c, alph=0.5):
     u = np.linspace(0, 2 * np.pi, 100)
@@ -137,6 +234,8 @@ def plot_sphere(ax, r, c, alph=0.5):
     # plot sphere with transparency
     ax.plot_surface(x, y, z, alpha=alph, color=c)
 
+
+
 def plot3Dtraj(traj, sphere_rad = 1.5e-4, title="", nmax = -1, zoom_range=5e-3):
 
     ### plot the trajoectory in 3D
@@ -146,7 +245,7 @@ def plot3Dtraj(traj, sphere_rad = 1.5e-4, title="", nmax = -1, zoom_range=5e-3):
     # Create a 3D plot
     fig = plt.figure(figsize=(12,9), facecolor='white', layout='constrained')
 
-    subfigs = fig.subfigures(2, 1, hspace=0.07, height_ratios=[2.5, 1])
+    subfigs = fig.subfigures(2, 1, hspace=0.07, height_ratios=[2, 1])
 
     ax3d = subfigs[0].add_subplot(1,2,1, projection='3d')
     ax3d_in = subfigs[0].add_subplot(1,2,2, projection='3d')
@@ -158,7 +257,7 @@ def plot3Dtraj(traj, sphere_rad = 1.5e-4, title="", nmax = -1, zoom_range=5e-3):
     cb=ax3d.scatter3D(traj[:nmax,0], traj[:nmax,1], traj[:nmax,2], c=cvec, cmap=cmap, s=1, ls='--')
     ax3d.plot(traj[:nmax,0], traj[:nmax,1], traj[:nmax,2], 'k', lw=1, alpha=0.4)
 
-    cbar = plt.colorbar(cb)
+    cbar = plt.colorbar(cb, fraction=0.04)
     cbar.set_label('Time [s]')
 
     # Add labels and title
@@ -180,7 +279,7 @@ def plot3Dtraj(traj, sphere_rad = 1.5e-4, title="", nmax = -1, zoom_range=5e-3):
     cb=ax3d_in.scatter3D(traj[nsamples:,0], traj[nsamples:,1], traj[nsamples:,2], c=cvec, cmap=cmap, s=1, ls='--')
     ax3d_in.plot(traj[nsamples:,0], traj[nsamples:,1], traj[nsamples:,2], 'k', lw=1, alpha=0.4)
 
-    cbar = plt.colorbar(cb)
+    cbar = plt.colorbar(cb, fraction=0.04)
     cbar.set_label('Time [s]')
 
     plot_sphere(ax3d_in, sphere_rad, 'gray', alph=0.5)
@@ -197,3 +296,28 @@ def plot3Dtraj(traj, sphere_rad = 1.5e-4, title="", nmax = -1, zoom_range=5e-3):
     cl,ch = np.median(traj[nsamples:,2])-ax_extent/2, np.median(traj[nsamples:,2])+ax_extent/2
     ax3d_in.set_zlim(cl,ch)
     ax3d_in.set_title("Zoom to end of trajectory")
+
+    ## now make 2d plots
+    coord_list = ["X", "Y", "Z"]
+    for i in range(3):
+        ax2d[i].plot(traj[:,-1], traj[:,i], 'k', lw=0.5)
+        ax2d[i].set_xlabel("Time [s]")
+        ax2d[i].set_ylabel(coord_list[i] + " [cm]")
+
+        if(traj[0,i] > 0):
+            ax2d_ins = ax2d[i].inset_axes((0.5, 0.5, 0.4, 0.4))
+        else:
+            ax2d_ins = ax2d[i].inset_axes((0.5, 0.2, 0.4, 0.4))
+        ax2d_ins.plot(traj[nsamples:,-1]*1e3, traj[nsamples:,i]*1e4, 'k', lw=0.5)
+        #ax2d_ins.plot(traj[nsamples:,-1]*1e3, np.ones_like(traj[nsamples:,-1])*sphere_rad*1e4, ':', color='gray')
+        ax2d_ins.set_xlabel("Time [ms]")
+        ax2d_ins.set_ylabel(coord_list[i] + " [um]")
+
+
+def mathieu(y, t, b, a, q):
+    u, w = y
+
+    dudx = w
+    dwdx = -b*w + (a - 2*q * np.cos(2*t))*u
+
+    return [dudx, dwdx]
